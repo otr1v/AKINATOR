cb4d9ef973b9db849ddd087030a15d3e842e9e38
otr1v <100768041+otr1v@users.noreply.github.com>
2023-02-21 22:31:41 +0300

    Update akinator.cpp

--- akinator.cpp
+++ akinator.cpp
@@ -1,21 +1,18 @@
 
 #include "akinator.h"
 
+
 int main()
 {
     char str[] = "human";
-    Node node = {};
+    Node*  node = NULL;
     FILE* base = fopen("base.txt", "w");
     CHECK_ERR(base == NULL, "didnot open");
-    // printf ("%*c", 4, "am");
     int count = 0;
-    AddNode(&node, str);
-    FreeBuffer();
-    AddNode(&node, "fdkdfl");
-
-    // AddNode(&node, str);
-
-    SaveBase(base, &node, 4);
-    //PreorderPrint(&node);
+    CreateTree(&node);
+    printf("%p", node->text);
+    printf("%s\n", node->text);
+    AddNode(node);
+    SaveBase(base, node, 4);
     return 0;
 }
