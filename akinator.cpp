16d3494bb3b3d9b04424ada610e9f9856347337c
otr1v <100768041+otr1v@users.noreply.github.com>
2023-02-28 22:44:37 +0300

    Update akinator.cpp

--- akinator.cpp
+++ akinator.cpp
@@ -1,18 +1,12 @@
 
 #include "akinator.h"
 
-
 int main()
 {
-    char str[] = "human";
     Node*  node = NULL;
-    FILE* base = fopen("base.txt", "w");
+    FILE* base = fopen("base.txt", "a+");
     CHECK_ERR(base == NULL, "didnot open");
     int count = 0;
-    CreateTree(&node);
-    printf("%p", node->text);
-    printf("%s\n", node->text);
-    AddNode(node);
-    SaveBase(base, node, 4);
+    Aki(node);
     return 0;
 }
